f204ea7c238650735783857c7a7a8cbbbe52bda7
Bennett Hiibner <hiibner@gmail.com>
2016-06-13 13:26:37 -0400

    Add files via upload

---
+++ wrapper/wrapper-header.php
@@ -0,0 +1,17 @@
+<?php /* Wrapper Name: Header */ ?>
+<div class="row">
+	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+			<?php get_template_part("static/static-logo"); ?>
+		</div>
+	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-top-header.php">
+		<?php get_template_part("static/static-social-networks"); ?>
+		<?php get_template_part("static/static-top-header"); ?>
+	</div>
+</div>
+<div class="nav-wrap">
+	<div class="row">		
+		<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+			<?php get_template_part("static/static-nav"); ?>
+		</div>
+	</div>
+</div>
